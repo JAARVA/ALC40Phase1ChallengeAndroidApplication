e92850d55736c3fc8ce9b4aca906e993a0ec17cd
AAR <50629883+JAARVA@users.noreply.github.com>
2019-07-12 14:54:50 +0200

    ALC - challenge

--- app/src/main/java/com/aar/alc40phase1challenge/MyProfile.java
+++ app/src/main/java/com/aar/alc40phase1challenge/MyProfile.java
@@ -10,5 +10,6 @@ public class MyProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+        this.setTitle("My Profile");
     }
 }
